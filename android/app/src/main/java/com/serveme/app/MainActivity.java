d0be6e8d3cc246dbaa67ddb462b2157bfb6836ff
jacob <JAKOUBBAKHOUCHE50@GMAIL.COM>
2025-09-19 12:08:55 +0100

    Update MainActivity.java

--- android/app/src/main/java/com/serveme/app/MainActivity.java
+++ android/app/src/main/java/com/serveme/app/MainActivity.java
@@ -1,5 +1,20 @@
 package com.serveme.app;
 
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+// استيراد حزم الإضافات التي قمت بتثبيتها
+import com.capacitorfirebase.authentication.FirebaseAuthentication;
+import com.getcapacitor.community.facebooklogin.FacebookLogin;
+
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // هنا نقوم بتهيئة وتسجيل الإضافات عند بدء تشغيل التطبيق
+        // حتى يتمكن Capacitor من استخدامها
+        registerPlugin(FirebaseAuthentication.class); // إضافة المصادقة من Firebase (لتسجيل الدخول بجوجل)
+        registerPlugin(FacebookLogin.class);          // إضافة تسجيل الدخول بفيسبوك
+    }
+}
